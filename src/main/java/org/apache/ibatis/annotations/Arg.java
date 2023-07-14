f916de11743a76fd6478c7d22909b1c76e89e5c3
juhaoyuan <juhaoyuan@rivamed.cn>
2023-07-14 11:13:43 +0800

    删除版权信息

--- src/main/java/org/apache/ibatis/annotations/Arg.java
+++ src/main/java/org/apache/ibatis/annotations/Arg.java
@@ -1,18 +1,3 @@
-/*
- *    Copyright 2010-2022 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.annotations;
 
 import java.lang.annotation.Documented;
@@ -27,7 +12,7 @@ import org.apache.ibatis.type.TypeHandler;
 import org.apache.ibatis.type.UnknownTypeHandler;
 
 /**
- * The annotation that specify a mapping definition for the constructor argument.
+ * 改注解用于为构造函数指定映射定义，以便在结果映射中使用构造函数来创建对象。
  *
  * @see ConstructorArgs
  * @author Clinton Begin
--- src/test/java/org/apache/ibatis/parsing/XPathParserTest.java
+++ src/test/java/org/apache/ibatis/parsing/XPathParserTest.java
@@ -1,18 +1,3 @@
-/*
- *    Copyright 2010-2022 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       https://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
 package org.apache.ibatis.parsing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -258,7 +244,13 @@ class XPathParserTest {
 
   @Test
   public void testReadXml(){
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    InputStream resourceAsStream = classLoader.getResourceAsStream("logback-test.xml");
+    URL resource1 = classLoader.getResource("logback-test.xml");
 
+    XPathParser xPathParser = new XPathParser(resourceAsStream);
+    XNode xNode = xPathParser.evalNode("/configuration");
+    String name = xNode.getName();
   }
 
 }
